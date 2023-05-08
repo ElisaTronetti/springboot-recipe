9efcfa2f7ce273fcecf55fe287d2138e9e2b4a43
Elisa Tronetti <elisa.tronetti1@gmail.com>
2023-05-08 17:09:22 +0200

    Initialize spring boot projext

---
+++ src/main/java/com/etronetti/reciphint/ReciphintApplication.java
@@ -0,0 +1,13 @@
+package com.etronetti.reciphint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciphintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciphintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etronetti/reciphint/ReciphintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etronetti.reciphint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciphintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
